898134ec10b859ef527c912ca69acb000177fcbe
YeffyCodeGit <aditchakraborty2009@gmail.com>
2021-10-05 20:47:22 +0600

    Commented out views engine creation and usage; not needed for now

--- api/api.go
+++ api/api.go
@@ -3,29 +3,29 @@ package api
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/template/html"
 )
 
 func Start() error {
-	engine := html.New("./views", ".html")
+	// engine := html.New("./views", ".html")
 
 	app := fiber.New(fiber.Config{
-		Views: engine,
+		//	Views:         engine,
 		CaseSensitive: true,
 	})
 
 	app.Use(logger.New())
-	app.Static("/", "./static")
 
+	app.Static("/", "./static")
 
 	for _, r := range routes {
 		app.Get(r.Url, r.Callback)
 	}
 
-
 	err := app.Listen(":3000")
 
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
